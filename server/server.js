00b13aa0a99c7ab015b9dacef47e118723df2f1e
praveen7101994 <30315923+praveen7101994@users.noreply.github.com>
2019-08-14 09:49:42 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -8,18 +8,30 @@ var {User} = require('./models/user');
 var app = express();
 
 app.use(bodyParser.json());
+
 app.post('/todos', (req, res) => {
+	console.log(req.body.text)
 	var todo = new Todo({
 		text: req.body.text
 	});
 
-	todo.save().then((doc)=>{
+	todo.save().then((doc) => {
 		res.send(doc);
-	}, (e)=>{
+	}, (e) => {
 		res.status(400).send(e);
-	})
+	});
 });
 
-app.listen(3000, ()=>{
+app.get('/todos',(req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos})
+	}, (e) => {
+		res.status(400).send(e);
+	});
+});
+
+app.listen(3000, () => {
 	console.log('Started at port 3000');
 }); 
+
+module.exports = {app};
